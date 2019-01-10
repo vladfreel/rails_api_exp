aceb768b19b1043a11be0270da91dde7eac3c050
vladfreel <vladfreelmaster@gmail.com>
2019-01-10 11:00:22 +0200

    tests for todo object

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,4 @@
 class Todo < ApplicationRecord
-  validates_presence_of :title
+  validates_presence_of :title, :created_by
   has_many :items, dependent: :destroy
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,10 +41,14 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 RSpec.configure do |config|
   # [...]
   # add `FactoryBot` methods
   config.include FactoryBot::Syntax::Methods
+
+  config.include RequestSpecHelper, type: :request
 
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
